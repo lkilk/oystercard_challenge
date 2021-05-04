1729a04681013a2a5aae606687bbfb70c282c196
Liam Kilkenny <81643363+lkilk@users.noreply.github.com>
2021-05-04 16:22:07 +0100

    chapter 9

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,16 +1,36 @@
 class Oystercard
   attr_reader :balance
   MAX_LIMIT = 90
+  MIN_LIMIT = 1
 
   def initialize
     @balance = 0
     @max_limit = MAX_LIMIT
+    @in_journey = false
   end
 
   def top_up(money)
     fail "Max limit is £#{MAX_LIMIT}" if @balance + money > MAX_LIMIT
 
     @balance += money
+  end
+
+  def deduct(money)
+    @balance -= money
+  end
+
+  def touch_in
+    fail "balance too low" if balance < MIN_LIMIT
+
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
+  end
+
+  def in_journey?
+    @in_journey
   end
 
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -12,6 +12,10 @@ require 'oystercard'
 # As a customer
 # I want a maximum limit (of £90) on my card
 
+# In order to pay for my journey
+# As a customer
+# I need my fare deducted from my card
+
 describe Oystercard do
 
 it "new card has default balance of £0" do
@@ -23,10 +27,30 @@ it "adds money to card" do
   expect(subject.balance).to eq (10)
 end
 
-it "raises error if balance is over £90" do
+it "raises error if balance is over max limit" do
   subject.top_up(Oystercard::MAX_LIMIT)
-  expect { subject.top_up(1) }.to raise_error "Max limit is £#{Oystercard::MAX_LIMIT}"
+  expect{ subject.top_up(1) }.to raise_error "Max limit is £#{Oystercard::MAX_LIMIT}"
 end
 
+it "deducts money from the card" do
+  subject.top_up(10)
+  subject.deduct(3)
+  expect(subject.balance).to eq (7)
+end
+
+it "returns in journey when touched in" do
+  allow(subject).to receive (:balance) {Oystercard::MIN_LIMIT}
+  subject.touch_in
+  expect(subject).to be_in_journey
+end
+
+it "returns not in journey when touched out" do
+  subject.touch_out
+  expect(subject).not_to be_in_journey
+end
+
+it "retuns an error if balance is less than min limit" do
+  expect{ subject.touch_in }.to raise_error "balance too low"
+end
 
 end
